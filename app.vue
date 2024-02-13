4081dc36fa9581b563e6847692818d912a668e50
sousukegit <greenneosoul.sousuke@gmail.com>
2024-02-14 07:47:54 +0900

    API確認OK（WARNはでている）

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <h1>dekita!!!</h1>
+    <h1>dekita!!!awaitaaaa</h1>
+    <div>data:{{ greet }}</div>
+    <button @click="getHello()">hello</button>
   </div>
 </template>
+
+<script setup lang="ts">
+
+const greet = ref<string>("")
+greet.value="aa"
+async function getHello(){
+  const { data } = await useFetch('http://localhost:3000/api/v1/hello')
+  // // const getHello = async () =>{    
+  // //   await useFetch('/api/v1/hello');
+  // // }
+  greet.value = data.value
+  console.log(data)
+  console.log(data.value)
+}
+
+</script>
