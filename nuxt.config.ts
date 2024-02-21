43aa3717d224ff8ff0e2f714c717585fa85d8dbe
sousukegit <greenneosoul.sousuke@gmail.com>
2024-02-21 12:30:52 +0900

    nuxt起動良好なことを確認

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig:{
     public:{
-      // apiOrigin: process.env.NUXT_API_ORIGIN
-      apiOrigin: ""
+       apiOrigin: process.env.NUXT_API_ORIGIN
+      //apiOrigin: ""
     }
   }
 })
