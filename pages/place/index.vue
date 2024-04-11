9ea955099b2636d8507956d4641fafe1052fbbf2
sousukegit <greenneosoul.sousuke@gmail.com>
2024-04-12 07:07:16 +0900

    googlemaps/js-api-loaderのコンポーザブルを追加（インストールまだ）

--- pages/place/index.vue
+++ pages/place/index.vue
@@ -2,6 +2,7 @@
     <TheContainer>
         <!-- ここにはgooglemap載せる -->
     <AppH1>MAP</AppH1>
+    <div id="map">google map</div>
 
     <div v-if="places.length">
       <div v-for="(place,i) in places"
@@ -32,7 +33,6 @@
   </template>
   <script setup lang="ts">
     import { useAuthStore } from '~~/stores/auth';
-    import { useUserStore } from '~~/stores/user';
     import { useTagsStore } from '~~/stores/tags';
     import { useHistoryState } from 'vue-history-state'
 
@@ -58,10 +58,8 @@
               try {
               const response = await useGet('/api/v1/places',customHeaders);
               // 成功時の処理
-              console.log(response)
               places.value = response
               checkFloat(places.value)
-              console.log(places.value)
               } catch (error) {
               console.log(error)
               }
@@ -79,4 +77,64 @@
          e.avg_reviews.rating = Number.isInteger(e.avg_reviews.rating) ? String(e.avg_reviews.rating)+".0" : String(e.avg_reviews.rating)
         });
     }
+
+    //spaで空のオブジェクトを宣言
+ // const plantMap = ref<google.maps.places.PlaceResult>({})
+
+  // onMounted(()=>{
+  //     const loader = useGoogle()
+  //     loader.load().then((google)=>{
+  //                 //mapインスタンス作成 初期描画用
+  //                 const map = new google.maps.Map(document.getElementById('map') ,{
+  //                     //初期表示設定
+  //                     //適当な値で表示
+  //                     zoom:17,
+  //                     center: new google.maps.LatLng(0,0),
+  //                     fullscreenControl:false,
+  //                     mapTypeControl:false,
+  //                     streetViewControl:true,
+  //                     streetViewControlOptions:{
+  //                         position:google.maps.ControlPosition.LEFT_BOTTOM
+  //                     },
+  //                     scaleControl:true
+  //                 })
+                  // const service = new google.maps.places.PlacesService(map)
+
+                  //受け取ったパラメータを元にmap情報の詳細を検索
+                  //getDetails（A,B）
+                  //Aでリクエスト送る
+                  //Bで戻ってきた関数の実行
+                  // service.getDetails({
+                  //     placeId:queryPlaceID
+                  // },
+                  
+                  //     (place,status)=>{
+                  //         if(status === google.maps.places.PlacesServiceStatus.OK){
+                  //             spa.value = place
+                  //             useHead({title: spa.value.name})
+                  //             //位置情報を取得
+                  //             map.setCenter(
+                  //                 new google.maps.LatLng(
+                  //                 place.geometry.location.lat(),
+                  //                 place.geometry.location.lng()
+                  //                 )
+                  //             )
+                  //             //マーカーオブジェクトをつける
+                  //             new google.maps.Marker({
+                  //                 map,
+                  //                 position: new google.maps.LatLng(
+                  //                     place.geometry.location.lat(),
+                  //                     place.geometry.location.lng()
+                  //                 )
+                  //             })
+                  //         }
+                  //     }
+                  // )
+
+  //             }
+  //         )
+  //         .catch(()=>{})
+  // })
+
+
   </script>
