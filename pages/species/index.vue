36f74a4dbdb1af79ef7c0012b9ce4328871b5cf0
sousukegit <greenneosoul.sousuke@gmail.com>
2024-05-26 08:11:44 +0900

    TODOコメント記載

--- pages/species/index.vue
+++ pages/species/index.vue
@@ -4,6 +4,8 @@
 <template>
   <TheContainer>
     <AppH1>種類別</AppH1>
+    <!-- ここに植物アイコンを並べたい -->
+    <div>アイコンを選んでください</div>
     <AppIcon :src="assets/images/man-like.png"></AppIcon>
   </TheContainer>
 </template>
