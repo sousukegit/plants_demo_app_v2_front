cf3ad48886afc19940f073e067b7bec30aece8e4
sousukegit <greenneosoul.sousuke@gmail.com>
2024-05-27 09:59:13 +0900

    植物別検索フォーム検討中

--- pages/species/index.vue
+++ pages/species/index.vue
@@ -1,11 +1,14 @@
 <script>
+import { ButtonSecondary } from '#build/components';
+
 
 </script>
 <template>
   <TheContainer>
     <AppH1>種類別</AppH1>
     <!-- ここに植物アイコンを並べたい -->
-    <div>アイコンを選んでください</div>
+    <input/>
+    <ButtonSecondary>検索</ButtonSecondary>
     <AppIcon :src="assets/images/man-like.png"></AppIcon>
   </TheContainer>
 </template>
