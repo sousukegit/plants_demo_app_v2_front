8446b00a369f29cec5e6fd891166e8cae430661a
sousukegit <greenneosoul.sousuke@gmail.com>
2024-08-30 21:06:18 +0900

    usePostを使ってより見やすくする

--- plugins/refreshTokenInit.client.ts
+++ plugins/refreshTokenInit.client.ts
@@ -3,40 +3,20 @@ import { useAuthStore } from '~~/stores/auth';
 //リロード時や初期アクセス時にリフレッシュトークンを持っていたらログイン状態にする
 //セッション情報が改ざんされた場合、Rails側でセッション情報はさくじょ
 export default defineNuxtPlugin( async () =>{
-    console.log("plugins refreshTokenInit")
-    const config = useRuntimeConfig()
-    console.log("fin runtimeconfig")
     const auth = useAuthStore();
-    console.log("fin useAuthStore");
-     const refresh = async() =>{
-        console.log("start $fetch");
-        await $fetch(
-           config.public.apiOrigin+'/api/v1/auth_token/refresh',
-          {
-              method:"POST",
-              credentials: 'include',
-              body:{},
-              headers:{
-                  'X-Requested-With': 'XMLHttpRequest',
-                  'Authorization': `Bearer ${auth.auth.token}`,
-              },
-          }
-          ).then(response =>{
-            console.log("fin $fetch");
-            auth.setAuth(response)
-            console.log(response)
-            //ログイン状態であればログイントップにリダイレクト
-            navigateTo('/main')
-          }).catch(error=>{
+    const customHeaders = {
+        'Authorization': `Bearer ${auth.auth.token}`
+    };
+    const refresh = async() =>{
+        try {
+            const response = await usePost('/api/v1/auth_token/refresh',{},customHeaders);
+            auth.setAuth(response);
+            navigateTo('/main');
+        } catch (error) {
+            console.log(error);
+        }
 
-          })
     }
-
-    async function initFunc(){
-        console.log("start initFunc");
-        const response = await refresh()
-    }
-
-    await initFunc()
-    console.log("fin initFunc");
+    
+    refresh();
 })
