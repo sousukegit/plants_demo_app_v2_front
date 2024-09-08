58d49324b2e2aae81a8546a3f1b356a7cbfae8f3
sousukegit <greenneosoul.sousuke@gmail.com>
2024-09-08 09:06:29 +0900

    関数再利用の為composables追加

---
+++ composables/api/useLogin.ts
@@ -0,0 +1,21 @@
+import type { authLoginForm } from '~/types/authLoginForm'
+import type { loginResponse } from '~/types/loginResponse'
+import type { errorResponse } from '~/types/errorResponse'
+
+export const useLogin = () => {
+
+  
+    const login = async(user:authLoginForm, authSuccessful:(response:loginResponse)=>void, authFailure:(error:errorResponse)=>void ):Promise<void> => {
+        try {
+        const response = await usePost<authLoginForm,loginResponse>('/api/v1/auth_token',user);
+          // 成功時の処理
+          if("token" in response){
+            authSuccessful(response)
+          }
+        } catch (error) {
+            authFailure(error as errorResponse)
+        }
+    }
+    return { login }
+}
+
