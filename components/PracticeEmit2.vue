8af7a276eb843f9d6630364672b3c811b379596c
sousukegit <greenneosoul.sousuke@gmail.com>
2024-10-15 16:28:24 +0900

    emits分かった

---
+++ components/PracticeEmit2.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+    type Props = {
+        randomNum:number;
+    }
+    const props = defineProps<Props>();
+    const randomNum = ref<number>(props.randomNum);
+
+    type Emits = {
+        (e:"emitFire",randomNum:number):void;
+    }
+
+    const emits = defineEmits<Emits>();
+
+    const culcRandom = () => {
+        randomNum.value = Math.random();
+        emits("emitFire",randomNum.value );
+    }
+
+
+</script>
+
+<template>
+    <div>
+        <div>子コンポーネント：{{ randomNum }}</div>
+        <button class="border bg-amber-50" @click="culcRandom">子で乱数発生</button>
+    </div>
+</template>
--- pages/practice.vue
+++ pages/practice.vue
@@ -10,6 +10,12 @@ const setNewCount = (newCount:number) =>{
     count.value = newCount;
 }
 
+
+const randomNum = ref<number>(0);
+const setRandomNum= (culcRandomNum:number) => {
+    randomNum.value = culcRandomNum;
+}
+
 </script>
 
 <template>
@@ -26,10 +32,16 @@ const setNewCount = (newCount:number) =>{
         <PracticeProps name="こくのある深い味黒" color="black"></PracticeProps>
     </div> -->
 
-    <div class="bg-sub-100">
+    <div class="bg-sub-100 mb-3">
         <p>emit</p>
         <div>親コンポーネントのカウント:{{ count }}</div>
         <PracticeEmit :count="count" @countup="setNewCount"></PracticeEmit>
+    </div>
+
+    <div>
+        <div>emits2</div>
+        <div>親コンポーネント:{{ randomNum }}</div>
+        <PracticeEmit2 :randomNum="randomNum" @emitFire="setRandomNum"></PracticeEmit2>
     </div>
     
 </template>
